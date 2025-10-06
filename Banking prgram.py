fcf951b0e81bed34bafc1374e48d4b031d830d5d
Sam <samradhyasingh123@gmail.com>
2025-10-06 22:40:59 +0530

    Add files via upload

---
+++ Banking prgram.py
@@ -0,0 +1,40 @@
+#python Banker
+balance = 0 
+is_running = True
+
+def show_balance():
+    print(f"Your balance is ${balance:.2f}")
+
+def deposit():
+    amount = float(input("Enter an amount to be deposited: $"))
+    if amount < 0:
+        print("That's not a valid amount")
+    else:
+        return amount
+
+def withdraw():
+    pass
+ 
+
+while is_running:
+    print("Hello! I am your virtual Banker")
+    print("--Please select one of the options--")
+    print("1. Deposit")
+    print("2 .Withdraw")
+    print("3 .Show balance")
+    print("4. Exit")
+
+    choice = input("enter your choice. (please Type the number only cause im a robot gng)")
+
+    if choice == '1':
+        balance += deposit()
+    elif choice == '2':
+        balance -= withdraw()
+    elif choice == '3':
+        show_balance()
+    elif choice == '4':
+        is_running = False
+    else:
+        print("That ain't no valid choice")
+
+
